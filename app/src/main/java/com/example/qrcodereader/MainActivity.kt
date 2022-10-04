332c48e75bf2d1678b2b78893addea630551bcfe
Deminda Thejan <demindathejan@gmail.com>
2022-10-04 21:05:26 +0530

    camera open reading qr code successfully

--- app/src/main/java/com/example/qrcodereader/MainActivity.kt
+++ app/src/main/java/com/example/qrcodereader/MainActivity.kt
@@ -5,18 +5,23 @@ import android.os.Bundle
 import android.widget.Toast
 import com.budiyev.android.codescanner.AutoFocusMode
 import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
 import com.budiyev.android.codescanner.DecodeCallback
 import com.budiyev.android.codescanner.ErrorCallback
 import com.budiyev.android.codescanner.ScanMode
+//import kotlinx.android.synthetic.main.activity_main.*
 
-import kotlinx.android.synthetic.main.activity_main.*
+//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
 private lateinit var codeScanner: CodeScanner
+    private lateinit var scannerView: CodeScannerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       scannerView = findViewById(R.id.scannerView)
 
         codeScanner = CodeScanner(this, scannerView)
         codeScanner.camera = CodeScanner.CAMERA_BACK
@@ -38,6 +43,9 @@ private lateinit var codeScanner: CodeScanner
 
         }
         codeScanner.startPreview()
+    }
+    fun checkPermission(){
+
     }
 
     override fun onResume() {
